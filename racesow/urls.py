4397b2114c1985036e86ee5333ba9b5b9405e9a3
Alex Hartsell <kalhartt@gmail.com>
2014-02-25 15:56:40 -0500

    Initial urls for racesow app

---
+++ racesow/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns
+from . import views
+
+urlpatterns = patterns(
+    '',
+    (r'^api/map/([A-Za-z0-9-_=]+)', views.APIMap.as_view()),
+    (r'^api/player/([A-Za-z0-9-_=]+)', views.APIPlayer.as_view()),
+    (r'^api/nick/([A-Za-z0-9-_=]+)', views.APINick.as_view()),
+    (r'^api/race', views.APIRace.as_view())
+)
