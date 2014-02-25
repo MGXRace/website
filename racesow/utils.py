6d87f8e8c920b45bf4f890a6677228d9a0031e91
Alex Hartsell <kalhartt@gmail.com>
2014-02-25 13:26:08 -0500

    Revert the + to space conversion on tokens

--- racesow/utils.py
+++ racesow/utils.py
@@ -4,4 +4,4 @@ import hashlib
 def authenticate(uTime, key, token):
     """Returns true if the token was generated from uTime and key"""
     h = hashlib.sha256("{}|{}".format(uTime, key))
-    return base64.b64encode(h.digest(), '-_') == token
+    return base64.b64encode(h.digest(), '-_') == token.replace(' ', '+')
