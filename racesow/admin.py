f8ce05848929e49c8d98b99355fcca619d53ec53
Alex Hartsell <kalhartt@gmail.com>
2014-02-25 15:38:52 -0500

    Register models w/ admin

--- racesow/admin.py
+++ racesow/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, Server, Map, MapRating, PlayerHistory
+from .models import Player, RaceHistory, Race, Checkpoint
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Server)
+admin.site.register(Map)
+admin.site.register(MapRating)
+admin.site.register(PlayerHistory)
+admin.site.register(Player)
+admin.site.register(RaceHistory)
+admin.site.register(Race)
+admin.site.register(Checkpoint)
